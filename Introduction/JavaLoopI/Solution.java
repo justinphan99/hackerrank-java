87b075719adf7617de81213a00e551035d973a32
justinphan99 <phanquockhoa99@gmail.com>
2022-12-23 00:16:48 +0700

    Update/Introduction/JavaLoopI

--- Introduction/JavaLoopI/Solution.java
+++ Introduction/JavaLoopI/Solution.java
@@ -1,5 +1,21 @@
 package Introduction.JavaLoopI;
 
+import java.io.*;
+
 public class Solution {
-    
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+        bufferedReader.close();
+        
+        if(N >= 2 && N <= 20){
+            for (int i = 1; i <= 10; i++) {
+                int res = N * i;
+                System.out.println(N + " x " + i + " = " + res);
+            }
+        }
+    }
 }
+
